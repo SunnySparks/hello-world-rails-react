96f36d276ef24c30e0b5b8c19509bd5fabfb9696
SunnySparks <Josediazt6@outlook.com>
2022-01-03 13:55:25 -0600

    Add an action and a reducer

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,17 +1,23 @@
 import React from "react"
-
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import configureStore from "../configureStore";
+import { Provider } from "react-redux";
 import HelloWorld from "./HelloWorld";
+const store = configureStore();
 
 class App extends React.Component {
   render () {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" render={() => ("Home!")} />
-          <Route path="/hello" render={() => <HelloWorld greeting="Friend"/>} />
-        </Switch>
-      </BrowserRouter>
+      <Provider store={store}>
+         <React.Fragment>
+           <Router>
+           <Routes>
+             <Route path="/" element= { ("Home!") }/>
+             <Route path="/hello" element={ <HelloWorld greeting="Friend" /> } />
+           </Routes>
+         </Router>
+         </React.Fragment>
+       </Provider>
     );
   }
 }
